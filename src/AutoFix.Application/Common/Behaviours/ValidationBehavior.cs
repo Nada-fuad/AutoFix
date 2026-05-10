80a27e1672bdf1b9f39e728f864dd2d243efb4ff
Nada-fuad <naan46511@gmail.com>
2026-05-10 21:31:51 +0200

    Refactor validation behavior

--- src/AutoFix.Application/Common/Behaviours/ValidationBehavior.cs
+++ src/AutoFix.Application/Common/Behaviours/ValidationBehavior.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
 
 namespace AutoFix.Application.Common.Behaviours
 {
-    internal class ValidationBehavior
+    public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : notnull
     {
+        private readonly IEnumerable<IValidator<TRequest>> _validators = validators;
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken ct)
+        {
+           if(_validators.Any())
+            {
+                return await next(ct);
+            }
+
+            var context = new ValidationContext<TRequest>(request);
+
+            var results= await Task.WhenAll(_validators.Select(v=>v.ValidateAsync(context,ct)));
+            var failures = results.SelectMany(r => r.Errors).Where(e=>e is not null).ToList();
+
+            
+            if (failures.Count != 0)
+            {
+                throw new ValidationException(failures);
+            }
+            return await next(ct);
+        }
     }
 }
